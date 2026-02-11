77b507724029dfd13ced9b4e460e4a2a53ce872a
kristofferLQ <kristofferqvenild@icloud.com>
2026-02-11 10:57:20 +0100

    Add Stock class with properties for symbol, company, and prices

---
+++ src/main/java/edu/ntnu/idi/idatt2003/Stock.java
@@ -0,0 +1,11 @@
+package edu.ntnu.idi.idatt2003;
+
+import java.util.List;
+import java.math.BigDecimal;
+
+public class Stock {
+    private String symbol;
+    private String cmopany;
+    private List<BigDecimal> prices;
+
+}
