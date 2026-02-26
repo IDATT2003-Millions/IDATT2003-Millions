f45c9dcb5c100af16864b60e6ea6769544bebe49
Leo <leo.pettersen@gmail.com>
2026-02-26 16:21:50 +0100

    feb26, completed draft for portfolio and TrasactionArchive, missing countDistinctWeeks in TransactionArchive

--- src/main/java/edu/ntnu/idi/idatt2003/model/TransactionArchive.java
+++ src/main/java/edu/ntnu/idi/idatt2003/model/TransactionArchive.java
@@ -1,6 +1,56 @@
 package edu.ntnu.idi.idatt2003.model;
 
-public class TransactionArchive {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Set;
 
-    private final transaction;
+public class TransactionArchive {
+    private List<Transaction> transactions;
+    public TransactionArchive() {
+        this.transactions = new ArrayList<>();
+    }
+
+    public boolean add(Transaction transaction) {
+        if (transaction == null) {
+            throw new IllegalArgumentException("Transaction cant be null");
+        }
+        return transactions.add(transaction);
+    }
+
+    public boolean isEmpty() {
+        return transactions.isEmpty();
+    }
+
+    public List<Transaction> getTransactions(int week) {
+        if (week <= 0) {
+            throw new IllegalArgumentException("Week must not be 0 or a negative number");
+        }
+        return transactions.stream()
+                .filter(transaction -> transaction.getWeek() == week)
+                .collect(Collectors.toList());
+    }
+
+    public List<Purchase> getPurchases(int week) {
+        if (week <= 0) {
+            throw new IllegalArgumentException("Week must not be 0 or a negative number.");
+        }
+        return transactions.stream()
+                .filter(transaction -> transaction instanceof Purchase && transaction.getWeek() == week)
+                .map(transaction -> (Purchase) transaction)
+                .collect(Collectors.toList());
+    }
+
+    public List<Sale> getSales(int week) {
+        if (week <= 0) {
+            throw new IllegalArgumentException("Week must not be 0 or a negative number.");
+        }
+        return transactions.stream()
+                .filter(transaction -> transaction instanceof Sale && transaction.getWeek() == week)
+                .map(transaction -> (Sale) transaction)
+                .collect(Collectors.toList());
+    }
+
+    public int countDistinctWeeks() {
+    }
 }
