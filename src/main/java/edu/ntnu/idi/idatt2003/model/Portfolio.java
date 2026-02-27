d5db92c8c132e34ca2f7333c61e733c018fbe4b8
kristofferLQ <kristofferqvenild@icloud.com>
2026-02-27 02:18:53 +0100

    Add javadoc to portfolio and checkstyle standards

--- src/main/java/edu/ntnu/idi/idatt2003/model/Portfolio.java
+++ src/main/java/edu/ntnu/idi/idatt2003/model/Portfolio.java
@@ -4,15 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ *Represents a players portfolio of shares.
  *
+ * <p>The portfolio stores all shares currently owned by a player.
+ * Shares can be added, removed, and queried by stock symbol.</p>
  */
 public class Portfolio {
   private List<Share> shares;
 
+  /**
+   * Creates an empty portfolio.
+   */
   public Portfolio() {
     this.shares = new ArrayList<>();
   }
 
+  /**
+   * Adds a share to portfolio.
+   *
+   * @param share the share to add
+   * @return true if the share was successfully added
+   * @throws IllegalArgumentException if the share is {@code empty}
+   */
   public boolean addShare(Share share) {
     if (share == null) {
       throw new IllegalArgumentException("Share cannot be empty");
@@ -21,6 +34,13 @@ public class Portfolio {
     return shares.add(share);
   }
 
+  /**
+   * Removes a share from portfolio.
+   *
+   * @param share the share to remove
+   * @return true if share is removed
+   * @throws IllegalArgumentException if share is {@code null}
+   */
   public boolean removeShare(Share share) {
     if (share == null) {
       throw new IllegalArgumentException("Share cannot be empty");
@@ -33,6 +53,13 @@ public class Portfolio {
     return List.copyOf(shares);
   }
 
+  /**
+   *Returns an immutable list of al shares in the portfolio.
+   *
+   * @param symbol the stock symbol to search for
+   * @return a copy of the portfolios share
+   * @throws IllegalArgumentException if symbol is {@code null} or {@code blank}
+   */
   public List<Share> getShares(String symbol) {
     if (symbol == null || symbol.isBlank()) {
       throw new IllegalArgumentException("Symbol cannot be blank or null");
@@ -43,6 +70,13 @@ public class Portfolio {
             .toList();
   }
 
+  /**
+   * Checks if the portfolio contains the given share.
+   *
+   * @param share the share to check
+   * @return true if the share exists in the portfolio
+   * @throws IllegalArgumentException if share is {@code null}
+   */
   public boolean contains(Share share) {
     if (share == null) {
       throw new IllegalArgumentException("Share cant be null");
