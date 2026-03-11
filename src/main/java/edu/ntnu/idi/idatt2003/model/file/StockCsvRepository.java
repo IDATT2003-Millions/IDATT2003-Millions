b5a6237d37baf9eaffb567135e0dc8ebff2e9d6f
kristofferLQ <kristofferqvenild@icloud.com>
2026-03-11 12:27:38 +0100

    made api for file handling

---
+++ src/main/java/edu/ntnu/idi/idatt2003/model/file/StockCsvRepository.java
@@ -0,0 +1,19 @@
+package edu.ntnu.idi.idatt2003.model.file;
+
+import edu.ntnu.idi.idatt2003.model.core.Stock;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.List;
+
+public class StockCsvRepository {
+
+    public List<Stock> load(Path path) throws IOException {
+        return null;
+    }
+
+    public void save(Path path, Collection<Stock> stocks) throws IOException {
+
+    }
+}
