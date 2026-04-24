eae7f3344b470e3fef0e2021fc9d70969dc5af1f
kristofferLQ <kristofferqvenild@icloud.com>
2026-04-24 13:52:00 +0200

    add onExchangeUpdated method to ExchangeObserver interface for exchange updates

--- src/main/java/edu/ntnu/idi/idatt2003/model/observer/ExchangeObserver.java
+++ src/main/java/edu/ntnu/idi/idatt2003/model/observer/ExchangeObserver.java
@@ -1,4 +1,7 @@
 package edu.ntnu.idi.idatt2003.model.observer;
 
+import edu.ntnu.idi.idatt2003.model.core.Exchange;
+
 public interface ExchangeObserver {
+  void onExchangeUpdated(Exchange exchange);
 }
