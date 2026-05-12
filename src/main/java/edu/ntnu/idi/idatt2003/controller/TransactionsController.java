b4ebc15c8845702ccb3e236d9eaf25d98797803b
kristofferLQ <kristofferqvenild@icloud.com>
2026-05-12 12:50:03 +0200

    add TransactionsController to manage transaction logic and interactions

---
+++ src/main/java/edu/ntnu/idi/idatt2003/controller/TransactionsController.java
@@ -0,0 +1,31 @@
+package edu.ntnu.idi.idatt2003.controller;
+
+import edu.ntnu.idi.idatt2003.model.core.Exchange;
+import edu.ntnu.idi.idatt2003.model.transactions.Player;
+import edu.ntnu.idi.idatt2003.model.transactions.Transaction;
+import edu.ntnu.idi.idatt2003.view.TransactionsView;
+import javafx.scene.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionsController {
+
+  private final Exchange exchange;
+  private final Player player;
+  private final TransactionsView view;
+
+  public TransactionsController(Exchange exchange, Player player) {
+    this.exchange = exchange;
+    this.player = player;
+    this.view = new TransactionsView();
+  }
+
+  public Node buildContent() {
+    List<Transaction> all = new ArrayList<>();
+    for (int w = 1; w <= exchange.getWeek(); w++) {
+      all.addAll(player.getTransactionArchive().getTransactions(w));
+    }
+    return view.buildContent(all);
+  }
+}
